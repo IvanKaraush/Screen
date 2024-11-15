3b161f1c8fb401aa70120801406176be89821280
Har0n313 <144205713+Har0n313@users.noreply.github.com>
2024-11-15 15:55:59 +0200

    Added rectangle logic

--- Screen/MainWindow.xaml.cs
+++ Screen/MainWindow.xaml.cs
@@ -1,9 +1,15 @@
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Interop;
+using System.Windows.Media;
+using Screen.Services;
 using Application = System.Windows.Application;
+using Brush = System.Windows.Media.Brush;
+using Color = System.Drawing.Color;
 using KeyEventArgs = System.Windows.Input.KeyEventArgs;
 using MouseEventArgs = System.Windows.Input.MouseEventArgs;
+using Point = System.Windows.Point;
 
 namespace Screen;
 
@@ -22,14 +28,19 @@ public partial class MainWindow
     private readonly ScreenCaptureService _screenCaptureService;
     private bool _printScreenPressed;
 
-    private ClipService clipService;
-    public MainWindow()
+    private readonly ClipService _clipService;
+    private readonly DrawArrow _drawArrow;
+    private bool _isDrawingArrow;
+    private static readonly Color DrawingColor = Color.Black;
+    private readonly Brush _selectedColor = new SolidColorBrush(System.Windows.Media.Color.FromArgb(DrawingColor.A, DrawingColor.R, DrawingColor.G, DrawingColor.B));    public MainWindow()
+    
     {
         _trayIcon = new NotifyIcon();
         _screenCaptureService = new ScreenCaptureService();
         InitializeComponent();
         InitializeTrayIcon();
-        clipService = new ClipService(ImageCanvas);
+        _clipService = new ClipService(ImageCanvas);
+        _drawArrow = new DrawArrow(ImageCanvas, _selectedColor);
     }
 
     protected override void OnSourceInitialized(EventArgs e)
@@ -67,7 +78,7 @@ public partial class MainWindow
 
     private void ComponentDispatcher_ThreadPreprocessMessage(ref MSG msg, ref bool handled)
     {
-        if (msg.message == WmHotkey && (int)msg.wParam == HotkeyId && _printScreenPressed == false)
+        if (msg.message == WmHotkey && (int)msg.wParam == HotkeyId && !_printScreenPressed)
         {
             _printScreenPressed = true;
             foreach (var screen in _screenCaptureService.Screens)
@@ -143,6 +154,39 @@ public partial class MainWindow
     private void AddRectangleButton_Click(object sender, RoutedEventArgs e)
     {
         var startPosition = new Point(50, 50);
-        clipService.AddRectangle(startPosition);
+        _clipService.AddRectangle(startPosition);
+    }
+
+    private void DrawArrowButton_Click(object sender, RoutedEventArgs e)
+    {
+        _isDrawingArrow = true;
+    }
+
+    private void ImageCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (_isDrawingArrow)
+        {
+            Point startPoint = e.GetPosition(ImageCanvas);
+            _drawArrow.StartDrawing(startPoint);
+        }
+    }
+
+    private void ImageCanvas_MouseMove(object sender, MouseEventArgs e)
+    {
+        if (_isDrawingArrow && e.LeftButton == MouseButtonState.Pressed)
+        {
+            Point currentPoint = e.GetPosition(ImageCanvas);
+            _drawArrow.UpdateDrawing(currentPoint);
+        }
+    }
+
+    private void ImageCanvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+    {
+        if (_isDrawingArrow)
+        {
+            Point endPoint = e.GetPosition(ImageCanvas);
+            _drawArrow.EndDrawing(endPoint);
+            _isDrawingArrow = false;
+        }
     }
 }
