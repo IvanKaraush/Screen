76dfe49f94d50195c8560555ca325c826a7cf243
KaylethDADA <144205848+KaylethDADA@users.noreply.github.com>
2025-01-12 15:23:48 +0200

    feat: add autostart settings

---
+++ Screen/Services/StartupManager.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Win32;
+
+namespace Screen.Services;
+
+public static class StartupManager
+{
+    private const string AppName = "Screen";
+    private static readonly string ExecutablePath = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+    private static readonly string RegistryPath = @"Software\Microsoft\Windows\CurrentVersion\Run";
+
+    public static bool IsEnabled
+    {
+        get
+        {
+            using (var key = Registry.CurrentUser.OpenSubKey(RegistryPath, false))
+            {
+                return key?.GetValue(AppName) as string == ExecutablePath;
+            }
+        }
+    }
+
+    public static void Enable()
+    {
+        using (var key = Registry.CurrentUser.OpenSubKey(RegistryPath, true))
+        {
+            key?.SetValue(AppName, ExecutablePath);
+        }
+    }
+
+    public static void Disable()
+    {
+        using (var key = Registry.CurrentUser.OpenSubKey(RegistryPath, true))
+        {
+            key?.DeleteValue(AppName, false);
+        }
+    }
+}
---
+++ Screen/SettingsWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using Screen.Services;
+
+namespace Screen;
+
+public partial class SettingsWindow : Window
+{
+    public SettingsWindow()
+    {
+        InitializeComponent();
+        StartupCheckBox.IsChecked = StartupManager.IsEnabled;
+    }
+
+    private void Window_Closed(object sender, EventArgs e)
+    {
+        if (StartupCheckBox.IsChecked == true)
+        {
+            StartupManager.Enable();
+        }
+        else
+        {
+            StartupManager.Disable();
+        }
+    }
+}
